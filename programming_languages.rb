f349a0c70184447ea7b30fc87c3aed71e514e6a9
Christopher Kalfas <chriskalfas@yahoo.com>
2019-04-03 19:43:59 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,15 +1,82 @@
-def reformat_languages(languages)
-new_hash = {}
-languages.each do |style, language|
-  language.each do |language_name,type|
-    if new_hash[language_name] == nil 
-      new_hash[language_name] = type
-    end
-    new_hash[language_name][:style] = []
-    new_hash[language_name][:style] << style
-  end
-end
-new_hash[:javascript][:style] << :oo 
+def reformat_languages(languages_hash)
+  new_hash = {}
+  languages_hash.each do | style, langs|
+    langs.each do |lang, attribute|
+      if new_hash[lang] == nil
+        new_hash[lang] = attribute
+        new_hash[lang][:style] = []
+      end 
+      new_hash[lang][:style] << style
+    end 
+  end 
+  new_hash
+end 
 
-new_hash
-end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def reformat_languages(languages)
+# new_hash = {}
+# languages.each do |style, language|
+#   language.each do |language_name,type|
+#     if new_hash[language_name] == nil 
+#       new_hash[language_name] = type
+#     end
+#     new_hash[language_name][:style] = []
+#     new_hash[language_name][:style] << style
+#   end
+# end
+# new_hash[:javascript][:style] << :oo 
+
+# new_hash
+# end
