b72d8b10102f4e5529ca809fd060cc17e817f1aa
Christopher Kalfas <chriskalfas@yahoo.com>
2019-04-03 19:01:41 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,15 +1,15 @@
-def reformat_languages(languages)
-new_hash = {}
-languages.each do |style, language|
-  language.each do |language_name,type|
-    if new_hash[language_name] == nil 
-      new_hash[language_name] = type
-    end
-    new_hash[language_name][:style] = []
-    new_hash[language_name][:style] << style
-  end
-end
-new_hash[:javascript][:style] << :oo 
+# def reformat_languages(languages)
+# new_hash = {}
+# languages.each do |style, language|
+#   language.each do |language_name,type|
+#     if new_hash[language_name] == nil 
+#       new_hash[language_name] = type
+#     end
+#     new_hash[language_name][:style] = []
+#     new_hash[language_name][:style] << style
+#   end
+# end
+# new_hash[:javascript][:style] << :oo 
 
-new_hash
-end
+# new_hash
+# end
